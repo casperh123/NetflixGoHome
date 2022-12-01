e47c5cf5a36bf18fb8a3c8089bc8cad431847296
Qezert <bekr@itu.dk>
2022-12-01 11:35:31 +0100

    Changes to MediaCollection and technically test outputs too....

--- src/domain/MediaCollection.java
+++ src/domain/MediaCollection.java
@@ -4,21 +4,30 @@ import java.util.ArrayList;
 public class MediaCollection {
 
     private DataHandler mediaListManager;
-    private ArrayList<Media> media;
+    private List<Media> media;
     private String genre;
 
-    public MediaCollection sortByGenre(String genre) {
-        throw new UnsupportedOperationException();
+    public MediaCollection() throws IOException {
+        mediaListManager = new DataHandler();
+        media = mediaListManager.assembleMediaList();
+        genre = null;
+    }
+    public MediaCollection sortByGenre(String genre) throws IOException{
+        MediaCollection newCollection = new MediaCollection();
+        for (Media media : media) {
+            if (media.genres.contains(genre)) {
+                newCollection.media.add(media);
+            }
+        }
+        return newCollection;
     }
 
     public void sortByRating() {
         throw new UnsupportedOperationException();
     }
-
     public void sortByReleaseYear() {
         throw new UnsupportedOperationException();
     }
-
     public void sortByAlphabetical() {
         throw new UnsupportedOperationException();
     }
