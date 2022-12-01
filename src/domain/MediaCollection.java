2d275200fc638de077fa0a1d5769315fa23b4376
Qezert <bekr@itu.dk>
2022-12-01 16:04:07 +0100

    Changes to MediaCollection and adding a MediaCollectionTest. Might need review.

--- src/domain/MediaCollection.java
+++ src/domain/MediaCollection.java
@@ -1,6 +1,8 @@
 package domain;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class MediaCollection {
@@ -14,17 +16,32 @@ public class MediaCollection {
         media = mediaListManager.assembleMediaList();
         genre = null;
     }
+    public MediaCollection(String genre) throws IOException {
+        mediaListManager = new DataHandler();
+        media = mediaListManager.assembleMediaList();
+        this.genre = genre;
+    }
     public MediaCollection sortByGenre(String genre) throws IOException{
-        MediaCollection newCollection = new MediaCollection();
+        MediaCollection sortedCollection = new MediaCollection(genre);
+        // For the object "media" of type Media in the list "media", where the list is a field for MediaCollection
         for (Media media : media) {
-            if (media.genres.contains(genre)) {
-                newCollection.media.add(media);
+            if (media.getGenres().contains(genre)) {
+                sortedCollection.media.add(media);
             }
         }
-        return newCollection;
+        return sortedCollection;
     }
-    public MediaCollection sortByName(List<String> chosenMedia) {
-        throw new UnsupportedOperationException();
+    //TODO Check if handles everything
+    public MediaCollection sortByName(List<String> chosenMedia) throws IOException{
+        MediaCollection sortedCollection = new MediaCollection();
+        for (Media media : media) {
+            for (String title : chosenMedia) {
+                if (media.getTitle().equals(title)) {
+                    sortedCollection.media.add(media);
+                }
+            }
+        }
+        return sortedCollection;
     }
     public void sortByRating() {
         throw new UnsupportedOperationException();
@@ -32,7 +49,26 @@ public class MediaCollection {
     public void sortByReleaseYear() {
         throw new UnsupportedOperationException();
     }
-    public void sortByAlphabetical() {
-        throw new UnsupportedOperationException();
+
+    public DataHandler getMediaListManager() {
+        return mediaListManager;
+    }
+
+    public List<Media> getMedia() {
+        return media;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    //TODO Look at this beauty! Maybe also improve before final version
+    public void sortByAlphabetical() throws IOException{
+        List<String> titleList = new ArrayList<>();
+        for (Media media : media) {
+            titleList.add(media.getTitle());
+        }
+        Collections.sort(titleList);
+         media = sortByName(titleList).media;
     }
 }
